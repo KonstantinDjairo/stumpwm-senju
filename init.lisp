6aef2764e10ab39e27ce59fac6912a5b3e1b1aad
Konstantin <alexandroviski8299@gmail.com>
2023-08-30 00:10:12 -0400

    Update init.lisp

--- init.lisp
+++ init.lisp
@@ -137,8 +137,11 @@
 (define-key *top-map* (kbd "M-n") "exec mupdf /mnt/Data/Japanese_Resources/languages-study(japanesAndRussian)/岩波数学辞典\ 第４版\ --\ 日本数学会\ --\ 第４版\,\ 2007\ --\ 岩波書店\ --\ 9784000803090\ --\ 49eeb143a4aef2f90d66e9527ffe161c\ --\ Anna’s\ Archive.pdf") 
 
 
-;; i need anki to be always open
+;; i need anki & qbittorrent to be always open
 (run-shell-command "exec /home/ronnie/Downloads/anki/anki-2.1.66-linux-qt6/anki")
+(run-shell-command "qbittorrent")
+
+
 
 ;; frames
 (define-key *root-map* (kbd "x") "hsplit")

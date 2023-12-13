8a2db77edde27fec209c65b159739777f668d015
Konstantin <alexandroviski8299@gmail.com>
2023-12-12 23:41:42 -0400

    fix terminal dependency

--- misc/audio_server.sh
+++ misc/audio_server.sh
@@ -8,11 +8,10 @@ LOG_FILE="/tmp/navidrome_logfile.log"
 mkdir -p "$(dirname "$LOG_FILE")"
 
 # Check if Navidrome is already running
-if pgrep -x "navidrome" > /dev/null
-then
+if pgrep -x "navidrome" > /dev/null; then
     echo "Navidrome is already running. Exiting."
 else
-    # Run Navidrome command, redirecting output to the log file
-    navidrome --musicfolder "$MUSIC_FOLDER" > "$LOG_FILE" 2>&1 &
-    echo "Navidrome started."
+    # Run Navidrome command, redirecting output to the log file and detaching it from the terminal
+    setsid nohup navidrome --musicfolder "$MUSIC_FOLDER" > "$LOG_FILE" 2>&1 &
+    echo "Navidrome started and detached from the terminal."
 fi
